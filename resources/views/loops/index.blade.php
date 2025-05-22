218c2df9eb51662755612dcd9de99cad004a4c61
unknown <eduardororo96@gmail.com>
2025-05-22 10:16:44 +0200

    subida mañana jueves

--- resources/views/loops/index.blade.php
+++ resources/views/loops/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -10,21 +9,25 @@
 @endif
 
 @forelse ($loops as $item)
-    <div class="card mb-3">
-        <div class="card-body">
-            <h5>{{ $item->title }}</h5>
-            <p>{{ $item->description }}</p>
-            <p>
-                <strong>BPM:</strong> {{ $item->bpm ?? 'N/A' }} |
-                <strong>Tonalidad:</strong> {{ $item->key_signature ?? 'N/A' }}
-            </p>
-            <audio controls>
-                <source src="{{ asset('storage/' . $item->audio_path) }}" type="audio/mpeg">
-                Tu navegador no soporta la reproducción de audio.
-            </audio>
-        </div>
+<div class="card mb-3">
+    <div class="card-body">
+        <h5>{{ $item->title }}</h5>
+        <p>{{ $item->description }}</p>
+        <p>
+            <strong>BPM:</strong> {{ $item->bpm ?? 'N/A' }} |
+            <strong>Tonalidad:</strong> {{ $item->key_signature ?? 'N/A' }}
+        </p>
+        <audio controls>
+            <source src="{{ asset('storage/' . $item->audio_path) }}" type="audio/wav">
+            <source src="{{ asset('storage/' . $item->audio_path) }}" type="audio/mpeg">
+            <source src="{{ asset('storage/' . $item->audio_path) }}" type="audio/ogg">
+            <p>Tu navegador no soporta la reproducción de audio.</p>
+            <a href="{{ asset('storage/' . $item->audio_path) }}">Descargar</a>
+            Tu navegador no soporta la reproducción de audio.
+        </audio>
     </div>
+</div>
 @empty
-    <p>No hay loops disponibles.</p>
+<p>No hay loops disponibles.</p>
 @endforelse
 @endsection
